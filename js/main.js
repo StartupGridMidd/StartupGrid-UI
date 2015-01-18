1d98fc3be7e19f0c3a8ab4ed3e99b39e7b2aa2c1
Will Potter <willstonepotter@gmail.com>
2015-01-18 17:05:41 -0500

    merging in some templating stuff

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var $ = require('jquery');
 var Sidebar = require("./sidebar");
+var SubtopicList = require("./subtopic-list");
 
 $(document).ready(function() {
-  console.log("jQuery loaded");
-  var sidebar = new Sidebar({
-    $el: $("#sidebar")
-  });
+  console.log("Initialize Page");
+  var sidebar = new Sidebar({});
+  var subtopicList = new SubtopicList({});
 });
