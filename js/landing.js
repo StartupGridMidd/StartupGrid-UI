4d046908f4aba5622bb4aee5ac623c51538582bc
Will Potter <willstonepotter@gmail.com>
2015-01-18 20:38:48 -0500

    adding a bunch of changes

---
+++ js/landing.js
@@ -0,0 +1,40 @@
+var $ = require("jquery");
+var Backbone = require("backbone");
+var _ = require("underscore");
+var hogan = require("hogan.js");
+Backbone.$ = $;
+var sampleData = { topics: [
+  {id: 2, name: "Operations"},
+  {id: 2, name: "Entrepeneurship"},
+  {id: 2, name: "Finance"},
+  {id: 2, name: "Stategy"},
+  {id: 2, name: "Venture Funding"},
+  {id: 2, name: "Leadership & Management"},
+  {id: 2, name: "Marketing & Sales"},
+  {id: 2, name: "Legal "}]
+};
+var LandingView = Backbone.View.extend({
+  initialize: function() {
+    console.log("loading landing view");
+    this.render();
+  },
+  events: {
+    "click .subtopic-card": "goToTopic",
+    "keypress .search-input": "search"
+  },
+  search: function() {
+    console.log(arguments);
+  },
+  goToTopic: function() {
+    console.log(arguments);
+  },
+  template: function(obj) {
+    return hogan.compile($("#template-landing").html()).render(obj);
+  },
+  render: function() {
+    this.$el.html(this.template(sampleData));
+    this.$el.addClass('landing');
+  }
+});
+
+module.exports = LandingView;
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 var $ = require('jquery');
 var Sidebar = require("./sidebar");
 var SubtopicList = require("./subtopic-list");
+var LandingView = require("./landing");
 
 $(document).ready(function() {
   console.log("Initialize Page");
-  var sidebar = new Sidebar({});
-  var subtopicList = new SubtopicList({});
+  var landingView = new LandingView({
+    el: "#container"
+  });
+  // var sidebar = new Sidebar({});
+  // var subtopicList = new SubtopicList({});
 });
